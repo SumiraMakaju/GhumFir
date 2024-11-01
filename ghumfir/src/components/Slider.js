651c2eb15c864f230b9ac456f47dfff91d200d91
Akash-kafle <aakashkafle47@gmail.com>
2024-11-01 23:44:27 +0545

    few changes to the data receiving

--- ghumfir/src/components/Slider.js
+++ ghumfir/src/components/Slider.js
@@ -3,6 +3,22 @@
 import { useState, useEffect, useCallback } from 'react';
 import styles from './slider.module.css';
 
+// we can just call a function for fetcing data from backend with what is needed
+/*
+const [slides, setSlides] = useState([]);
+
+const getdata = {
+let url = 'http://localhost:3000/slides';
+return fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    return data;
+  }).catch(error => {
+    console.error('Error:', error);
+    return [];
+    };
+    }
+*/
 const slides = [
   {
     image: '/slide2.jpg',
@@ -32,19 +48,48 @@ const slides = [
   {
     image: '/bhaktapur-city.webp',
     name: 'Bhaktapur',
-    description: 'Vibrant landscapes of Pokhara.',
+    description: 'Rich culture of traditional Newari city.',
   },
-  
-  
   {
     image: '/tilicho.jpg',
     name: 'Tilicho Lake',
-    description: 'Vibrant landscapes of Pokhara.',
+    description: 'Picturesque view of Tilicho Lake.',
   },
 
 ];
 
 const Slider = () => {
+  /**
+   * // This should work
+   * 
+   * useEffect(()=>
+   * {
+   *  getdata.then(data => setSlides(data));
+   * },[]);
+   * 
+   * //  setting slide limit to 7 for now
+   * //  {pretty sure this can be done in a better way}
+   * 
+   * let ([pic,setPic],i) = (useState([]),-1);
+   * const put = slides.map((slide,index) => {
+   * if(i<7 && i<slides.image && slide.image){
+   * setPic(pic => [...pic,slide]);
+   * i++;
+   * }
+   * });
+   * 
+   * put();
+   * 
+   * //  when next is clicked put will be called again for next 7 images (number is arbitrary)
+   * 
+   * const handleNew = useCallback(() => {
+   * setPic([]);
+   * put();
+   * });
+   * 
+   * // after this you can do your magic front end stuff 
+   * 
+   */
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleNext = useCallback(() => {
@@ -61,8 +106,8 @@ const Slider = () => {
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === 'ArrowRight') handleNext();
-      if (event.key === 'ArrowLeft') handlePrev();
+      if (event.key === 'ArrowRight' || event.key === 'ArrowDown') handleNext();
+      if (event.key === 'ArrowLeft' || event.key === 'ArrowUp') handlePrev();
     };
 
     window.addEventListener('keydown', handleKeyDown);
